ed0f616e8670ccab97f8886a910abba1e747b605
Santiago de Pedro <santiago.pedro23@gmail.com>
2020-03-11 18:15:56 -0400

    Adding frontend project.

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.scss';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faCoffee} from '@fortawesome/free-solid-svg-icons'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <FontAwesomeIcon icon={faCoffee}/>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
