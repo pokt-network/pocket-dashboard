17293276619d14592b82130382c0095310d5aae6
Roniel Valdez <roniel_valdez@outlook.com>
2020-03-18 11:16:08 -0400

    Create Sign up view

--- frontend/src/_routes.js
+++ frontend/src/_routes.js
@@ -3,28 +3,51 @@ import Dashboard from "./views/Core/Dashboard/Dashboard";
 import ForgotPassword from "./views/Core/ForgotPassword/ForgotPassword";
 import GithubAuthProviderHook from "./core/components/providers/auth/GithubAuthProviderHook";
 import GoogleAuthProviderHook from "./core/components/providers/auth/GoogleAuthProviderHook";
+import SignUp from "./views/Core/SignUp/SignUp";
 
 export const routePaths = {
+  signup: "/signup",
   login: "/login",
   home: "/dashboard",
   forgot_password: "/forgot-password"
-}
+};
 
 /**
  * @type {Array<{path: string, component: Component, exact: boolean, name: string}>}
  */
 const pageRoutes = [
-  {path: routePaths.login, exact: true, name: "Login", component: Login},
-  {path: routePaths.home, exact: true, name: "Dashboard", component: Dashboard},
-  {path: routePaths.forgot_password, exact: true, name: "Forgot Passsword", component: ForgotPassword}
+  { path: routePaths.login, exact: true, name: "Login", component: Login },
+  {
+    path: routePaths.home,
+    exact: true,
+    name: "Dashboard",
+    component: Dashboard
+  },
+  {
+    path: routePaths.forgot_password,
+    exact: true,
+    name: "Forgot Passsword",
+    component: ForgotPassword
+  },
+  { path: routePaths.signup, exact: true, name: "Sign Up", component: SignUp }
 ];
 
 /**
  * @type {Array<{path: string, component: Component, exact: boolean, name: string}>}
  */
 const authProviderRoutes = [
-  {path: "/api/auth/provider/github", exact: true, name: "Github", component: GithubAuthProviderHook},
-  {path: "/api/auth/provider/google", exact: true, name: "Google", component: GoogleAuthProviderHook},
+  {
+    path: "/api/auth/provider/github",
+    exact: true,
+    name: "Github",
+    component: GithubAuthProviderHook
+  },
+  {
+    path: "/api/auth/provider/google",
+    exact: true,
+    name: "Google",
+    component: GoogleAuthProviderHook
+  }
 ];
 
 const routes = pageRoutes.concat(authProviderRoutes);
---
+++ frontend/src/views/Core/SignUp/SignUp.js
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import { AuthProviderButton } from "../../../core/components/AuthProviderButton";
+import Help from "../../../core/components/Help/Help";
+import Sidebar from "../../../core/components/Sidebar";
+import UserService from "../../../core/services/PocketUserService";
+import "../Login/Login.scss";
+import { routePaths } from "../../../_routes";
+
+class SignUp extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      authProviders: []
+    };
+  }
+
+  componentDidMount() {
+    /** @type {UserService} */
+    UserService.getAuthProviders().then(providers => {
+      this.setState({ authProviders: providers });
+    });
+  }
+
+  // TODO: Refactor logic from providers to be shared by both sign up and
+  // login
+  /**
+   * @param {string} name Name of auth provider.
+   *
+   * @return {{name: string, consent_url:string}}
+   * @private
+   */
+  __getAuthProvider(name) {
+    /** @type {Array.<{name:string, consent_url:string}>} */
+    const authProviders = this.state.authProviders;
+
+    return authProviders.filter(
+      value => value.name.toLowerCase() === name.toLowerCase()
+    )[0];
+  }
+
+  render() {
+    const { login } = routePaths;
+    return (
+      <Container fluid id={"login-page"}>
+        <Row>
+          <Sidebar />
+          <Col id={"content"}>
+            <Help />
+
+            <div id={"main"}>
+              <h1>Create Account</h1>
+              <div id={"provider-buttons"}>
+                <AuthProviderButton
+                  authProvider={this.__getAuthProvider("google")}
+                />
+                <AuthProviderButton
+                  authProvider={this.__getAuthProvider("github")}
+                />
+              </div>
+              <hr />
+              <Form id={"main-form"}>
+                <Form.Group>
+                  <Form.Label>E-mail</Form.Label>
+                  <Form.Control type="email" />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Confirm password</Form.Label>
+                  <Form.Control type="password" />
+                </Form.Group>
+
+                <Button type="submit" variant="dark" size={"lg"} block>
+                  Login
+                </Button>
+                <div>
+                  <Link to={login}>Do you have an account?</Link>
+                </div>
+              </Form>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default SignUp;
