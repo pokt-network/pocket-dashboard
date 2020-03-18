2c6077eb09118d70e2e22563b1e765198241309f
Roniel Valdez <roniel_valdez@outlook.com>
2020-03-18 12:56:29 -0400

    Refactor sidebar to allow children

--- frontend/src/core/components/Sidebar.js
+++ frontend/src/core/components/Sidebar.js
@@ -1,31 +1,18 @@
-import React, {Component} from 'react';
-import {Col, Row} from 'react-bootstrap'
-
+import React, { Component } from "react";
+import { Col, Row } from "react-bootstrap";
 
 class Sidebar extends Component {
-  render() { 
-    return ( 
+  render() {
+    const { children } = this.props;
+
+    return (
       <Col xs={2} sm={3} lg={3} id={"sidebar"}>
-            <Row>
-              <img src={"/logo.png"} alt="logo" id={"main-logo"}/>
-            </Row>
-            <Row id={"title"}>
-              <h1>
-                We are <br/>
-                pocket <br/>
-                network
-              </h1>
-            </Row>
-            <Row>
-              <p>
-                {/* eslint-disable-next-line react/no-unescaped-entities */}
-                Pocket Network's mission is to ensure the sustainable <br/>
-                Decentralization of blockchain infrastructure. In a <br/>
-                market that is over-reliant on single-service provider.
-              </p>
-            </Row>
-          </Col>
-     );
+        <Row>
+          <img src={"/logo.png"} alt="logo" id={"main-logo"} />
+        </Row>
+        {children}
+      </Col>
+    );
   }
 }
 
