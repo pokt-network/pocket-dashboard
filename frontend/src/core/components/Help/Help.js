b9edc05790b6f1fe9c622f3f679ecacd55c13942
Roniel Valdez <roniel_valdez@outlook.com>
2020-03-18 12:58:11 -0400

    Refactor help component

--- frontend/src/core/components/Help/Help.js
+++ frontend/src/core/components/Help/Help.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 import { Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
 
 import "./Help.scss";
 
-const Help = () => {
-  return (
-    <div className={"help"}>
-      <Button variant={"link"}>
-        <FontAwesomeIcon icon={faQuestionCircle} />
-      </Button>
-    </div>
-  );
+export default class Help extends Component {
+  render() {
+    const { link, className } = this.props;
+
+    return (
+      <div className={className}>
+        <Link to={link}>
+          <Button variant={"link"}>
+            <FontAwesomeIcon icon={faQuestionCircle} />
+          </Button>
+        </Link>
+      </div>
+    );
+  }
+}
+
+Help.defaultProps = {
+  link: "#",
+  className: "help"
 };
 
-export default Help;
+Help.propTypes = {
+  link: PropTypes.string,
+  className: PropTypes.string
+};
