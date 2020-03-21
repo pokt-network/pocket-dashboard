a57a4e62ce8d3b27d50cd074bf978d8e3ba1bad2
Santiago de Pedro <santiago.pedro23@gmail.com>
2020-03-21 09:54:28 -0400

    Add security question service at frontend.

---
+++ frontend/src/core/services/PocketSecurityQuestionsService.js
@@ -0,0 +1,49 @@
+import PocketBaseService from "./PocketBaseService";
+import axios from "axios";
+
+class PocketSecurityQuestionsService extends PocketBaseService {
+
+  constructor() {
+    super("/api/security_questions");
+  }
+
+  /**
+   * Get security questions.
+   *
+   * @return {Promise|Promise<Array.<{string}>>}
+   */
+  getSecurityQuestions() {
+    return axios.get(this._getURL("/"))
+      .then(response => response.data);
+  }
+
+  /**
+   * Save Answers of security questions.
+   *
+   * @param {string} email Email of user.
+   * @param {Array<{question:string, answer:string}>} questions Security questions with their answers.
+   *
+   * @return {Promise|Promise<{success:boolean, [data]: *}>}
+   */
+  async saveSecurityQuestionAnswers(email, questions) {
+    const data = {
+      email,
+      questions
+    };
+
+    return axios.post(this._getURL("answered"), data)
+      .then(response => {
+        if (response.status === 200) {
+
+          return {success: response.data};
+        }
+
+        return {success: false};
+      }).catch(err => {
+        return {success: false, data: err};
+      });
+  }
+
+}
+
+export default new PocketSecurityQuestionsService();
