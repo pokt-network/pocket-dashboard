d7a131a62c3c008cb00000053bc8a701c5a11163
Roniel Valdez <roniel_valdez@outlook.com>
2020-03-25 12:56:12 -0400

    Rename file and delete unused code

--- frontend/src/core/components/InfoCard/InfoCard.js
+++ frontend/src/core/components/InfoCard/InfoCard.js
@@ -1,27 +1,25 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import "./StatusBadge.scss";
+import "./InfoCard.scss";
 
-class StatusBadge extends Component {
+class InfoCard extends Component {
   render() {
     const { title, subtitle } = this.props;
 
     return (
-      // <div className="status">
       <div className="p-badge">
         <div className="p-badge-body">
           <h1>{title}</h1>
           <p>{subtitle}</p>
         </div>
       </div>
-      // </div>
     );
   }
 }
 
-StatusBadge.propTypes = {
+InfoCard.propTypes = {
   title: PropTypes.string,
   subtitle: PropTypes.string
 };
 
-export default StatusBadge;
+export default InfoCard;
