403077e251970413a9a0210586218d9e87359b4a
Roniel Valdez <roniel_valdez@outlook.com>
2020-03-26 12:05:07 -0400

    Set up dashboard to show app skeleton

--- frontend/src/views/Dashboard/Dashboard.js
+++ frontend/src/views/Dashboard/Dashboard.js
@@ -1,22 +1,44 @@
 import React, {Component} from "react";
+import AppNavbar from "../../core/components/Dashboard/AppNavbar/AppNavbar";
 import UserService from "../../core/services/PocketUserService";
+import {Container, Col, Row} from "react-bootstrap";
+import AppSidebar from "../../core/components/Dashboard/AppSidebar/AppSidebar";
+import Breadcrumbs from "../../core/components/Dashboard/Breadcrumb";
 
 class Dashboard extends Component {
-
   render() {
     return (
-      <div>
-        <h1>User</h1>
-        <div>
-          <div>Name:</div>
-          <div>{UserService.getUserInfo().name}</div>
-          <div>Email:</div>
-          <div>{UserService.getUserInfo().email}</div>
-        </div>
-      </div>
+      <Container fluid className={"auth-page"}>
+        <Row>
+          <AppSidebar />
+          <Col>
+            <AppNavbar />
+            <Row>
+              {/* TODO: Remove manually written links for testing purposes */}
+              <Breadcrumbs
+                links={[
+                  {url: "#", label: "Home", active: false},
+                  {url: "#1", label: "Dashboard", active: false},
+                  {url: "#2", label: "Overview", active: true},
+                ]}
+              />
+            </Row>
+            <Row>
+              <div>
+                <h1>User</h1>
+                <div>
+                  <div>Name:</div>
+                  <div>{UserService.getUserInfo().name}</div>
+                  <div>Email:</div>
+                  <div>{UserService.getUserInfo().email}</div>
+                </div>
+              </div>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
 
 export default Dashboard;
-
