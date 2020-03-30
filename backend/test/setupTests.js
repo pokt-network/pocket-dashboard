6120e008b1ea6e5e93bb6d560a8b7019cce45199
Santiago de Pedro <santiago.pedro23@gmail.com>
2020-03-30 14:03:42 -0400

    Adding application functionality and application specs.

--- backend/test/setupTests.js
+++ backend/test/setupTests.js
@@ -1,10 +1,10 @@
 import {after, before} from "mocha";
-import MongoDBAdapter from "../../src/providers/data/db/MongoDBAdapter";
-import PersistenceProvider from "../../src/providers/data/PersistenceProvider";
-import {Configurations} from "../../src/_configuration";
+import MongoDBAdapter from "../src/providers/data/db/MongoDBAdapter";
+import PersistenceProvider from "../src/providers/data/PersistenceProvider";
+import {Configurations} from "../src/_configuration";
 import sinon from "sinon";
-import * as dbProvider from "../../src/providers/data/db";
-import * as pocket from "../../src/services/PocketService";
+import * as dbProvider from "../src/providers/data/db";
+import * as pocket from "../src/services/PocketService";
 
 /** @type {PersistenceProvider} */
 let persistenceService = null;
@@ -20,7 +20,6 @@ before(() => {
 
 
 after(async () => {
-
   await persistenceService.dropDataBase();
 
   mongoDBProvider = null;
