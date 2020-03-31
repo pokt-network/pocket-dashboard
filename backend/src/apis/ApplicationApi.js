f62de07332dad581daccf6c03fa279345944ecbb
Santiago de Pedro <santiago.pedro23@gmail.com>
2020-03-31 15:18:31 -0400

    Add get applications endpoints.

--- backend/src/apis/ApplicationApi.js
+++ backend/src/apis/ApplicationApi.js
@@ -6,6 +6,48 @@ const router = express.Router();
 const applicationService = new ApplicationService();
 
 /**
+ * @param {{query:object}} request Request.
+ * @param {string} option Option.
+ *
+ * @returns {string} Query option value.
+ */
+function getQueryOption(request, option) {
+  const parsedData = request.query;
+
+  // eslint-disable-next-line no-prototype-builtins
+  if (!parsedData.hasOwnProperty(option)) {
+    throw Error(`${option} query parameter is required.`);
+  }
+
+  if (parsedData[option] === undefined) {
+    throw Error(`${option} query parameter cannot be null.`);
+  }
+
+  return parsedData[option];
+}
+
+/**
+ * @param {{query:object}} request Request.
+ * @param {string} option Option.
+ *
+ * @returns {string} Query option value.
+ */
+function getOptionalQueryOption(request, option) {
+  const parsedData = request.query;
+
+  // eslint-disable-next-line no-prototype-builtins
+  if (!parsedData.hasOwnProperty(option)) {
+    return "";
+  }
+
+  if (parsedData[option] === undefined) {
+    return "";
+  }
+
+  return parsedData[option];
+}
+
+/**
  * Create new application.
  */
 router.post("/", async (request, response) => {
@@ -15,6 +57,53 @@ router.post("/", async (request, response) => {
     const application = await applicationService.createApplication(data);
 
     response.send(application);
+  } catch (e) {
+    const error = {
+      message: e.toString()
+    };
+
+    response.status(400).send(error);
+  }
+});
+
+/**
+ * Get all applications.
+ */
+router.get("", async (request, response) => {
+  try {
+
+    const limit = parseInt(getQueryOption(request, "limit"));
+    const offsetData = getOptionalQueryOption(request, "offset");
+    const offset = offsetData !== "" ? parseInt(offsetData) : 0;
+
+    const applications = await applicationService.getAllApplications(limit, offset);
+
+    response.send(applications);
+  } catch (e) {
+    const error = {
+      message: e.toString()
+    };
+
+    response.status(400).send(error);
+  }
+});
+
+/**
+ * Get all user applications.
+ */
+router.post("/user", async (request, response) => {
+  try {
+
+    const limit = parseInt(getQueryOption(request, "limit"));
+    const offsetData = getOptionalQueryOption(request, "offset");
+    const offset = offsetData !== "" ? parseInt(offsetData) : 0;
+
+    /** @type {{user: string}} */
+    const data = request.body;
+
+    const applications = await applicationService.getUserApplications(data.user, limit, offset);
+
+    response.send(applications);
   } catch (e) {
     const error = {
       message: e.toString()
