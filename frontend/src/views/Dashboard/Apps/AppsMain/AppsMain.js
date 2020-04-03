fc9f99179ac11d4afe4783683d42ac8328be042e
Roniel Valdez <roniel_valdez@outlook.com>
2020-04-03 09:53:21 -0400

    Resolve pull conflict

--- frontend/src/views/Dashboard/Apps/AppsMain/AppsMain.js
+++ frontend/src/views/Dashboard/Apps/AppsMain/AppsMain.js
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-import axios from "axios";
 import "./AppsMain.scss";
 import {Button, Col, Row, InputGroup, FormControl} from "react-bootstrap";
 import InfoCard from "../../../../core/components/InfoCard/InfoCard";
