0d86eab23659d1d4b7494d08a1a25e7ff08eccfd
Santiago de Pedro <santiago.pedro23@gmail.com>
2020-04-09 12:28:35 -0400

    Working with new card payment method form.

--- frontend/src/core/components/Payment/NewCardPaymentMethodForm.js
+++ frontend/src/core/components/Payment/NewCardPaymentMethodForm.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {Col, Row} from "react-bootstrap";
+import {Col, Form, Row} from "react-bootstrap";
 import PaymentContainer from "./PaymentContainer";
 
 class NewCardPaymentMethodForm extends Component {
@@ -11,14 +11,56 @@ class NewCardPaymentMethodForm extends Component {
           <h2>Add Payment method</h2>
         </Row>
 
-        <Row>
-          <Col>
+        <Form>
+          <Row>
+            <Col sm="6" md="6" lg="6">
+              Credit card number
+            </Col>
+            <Col sm="6" md="6" lg="6">
+              <Form.Group>
+                <Form.Label>Card holder name*</Form.Label>
+                <Form.Control
+                  name="cardHolderName"
+                  type="text"
+                />
+              </Form.Group>
+            </Col>
+          </Row>
 
-          </Col>
-          <Col>
+          <Row>
+            <Col sm="6" md="6" lg="6">
+              <Form.Group>
+                <Form.Label>Expiration Date*</Form.Label>
+                <Form.Control
+                  name="expirationDate"
+                  type="text"
+                />
+              </Form.Group>
+            </Col>
+            <Col sm="6" md="6" lg="6">
+              <Form.Group>
+                <Form.Label>CVC/CVC2*</Form.Label>
+                <Form.Control
+                  name="cvc"
+                  type="text"
+                />
+              </Form.Group>
+            </Col>
+          </Row>
 
-          </Col>
-        </Row>
+          <Row>
+            Billing Adddres Line 1
+          </Row>
+
+          <Row>
+            <Col sm="6" md="6" lg="6">
+              Zip Code
+            </Col>
+            <Col sm="6" md="6" lg="6">
+              Region/Country
+            </Col>
+          </Row>
+        </Form>
       </PaymentContainer>
     );
   }
