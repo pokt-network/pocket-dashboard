60485f8adcee582cb1c248097b6eeae5b8022cc7
Roniel Valdez <roniel_valdez@outlook.com>
2020-04-22 16:17:21 -0400

    Add modal to generalInformation view

--- frontend/src/views/Profile/General/General.js
+++ frontend/src/views/Profile/General/General.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {Button, Col, Form, Row} from "react-bootstrap";
+import {Button, Col, Form, Modal, Row} from "react-bootstrap";
 import PocketUserService from "../../../core/services/PocketUserService";
 
 class General extends Component {
@@ -14,6 +14,7 @@ class General extends Component {
         username: "",
         email: "",
       },
+      passwordModal: false,
     };
   }
 
@@ -38,40 +39,78 @@ class General extends Component {
 
   render() {
     const {username, email} = this.state.data;
+    const {passwordModal} = this.state;
 
     return (
-      <Row>
-        <Col lg="8" md="8" sm="8">
-          <h2>General Information</h2>
-          <Form onSubmit={this.handleSubmit}>
+      <>
+        <Row>
+          <Col lg="8" md="8" sm="8">
+            <h2>General Information</h2>
+            <Form onSubmit={(e) => e.preventDefault()}>
+              <Form.Group>
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                  name="username"
+                  value={username}
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>E-mail</Form.Label>
+                <Form.Control
+                  type="email"
+                  name="email"
+                  value={email}
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Button
+                className="pr-5 pl-5 float-right"
+                type="submit"
+                variant="dark"
+                size={"lg"}
+                onClick={() => {
+                  this.setState({passwordModal: true});
+                }}
+              >
+                Save
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+        <Modal
+          className="app-modal"
+          show={passwordModal}
+          onHide={() => this.setState({passwordModal: false})}
+          animation={false}
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Answer this question before continuing</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
             <Form.Group>
-              <Form.Label>Username</Form.Label>
+              {/* TODO: Add retrieve question from backend */}
+              <Form.Label>What was the name of your first child?</Form.Label>
               <Form.Control
                 name="username"
                 value={username}
                 onChange={this.handleChange}
               />
             </Form.Group>
-            <Form.Group>
-              <Form.Label>E-mail</Form.Label>
-              <Form.Control
-                type="email"
-                name="email"
-                value={email}
-                onChange={this.handleChange}
-              />
-            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
             <Button
-              className="pr-5 pl-5 float-right"
-              type="submit"
+              block
               variant="dark"
-              size={"lg"}
+              className="pr-4 pl-4"
+              onClick={this.handleSubmit}
             >
-              Save
+              Continue
             </Button>
-          </Form>
-        </Col>
-      </Row>
+          </Modal.Footer>
+        </Modal>
+      </>
     );
   }
 }
