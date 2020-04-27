18cf8e12499fe90638f10e9890799865ca7e4ca2
Santiago de Pedro <santiago.pedro23@gmail.com>
2020-04-27 14:49:37 -0400

    Adding create node API method.

---
+++ backend/src/apis/NodeApi.js
@@ -0,0 +1,33 @@
+import express from "express";
+import NodeService from "../services/NodeService";
+
+const router = express.Router();
+
+const nodeService = new NodeService();
+
+/**
+ * Create new node.
+ */
+router.post("", async (request, response) => {
+  try {
+    /** @type {{node: {name:string, contactEmail:string, user:string, owner:string, description:string, icon:string}, privateKey?:string}} */
+    let data = request.body;
+
+    if (!("privateKey" in data)) {
+      data["privateKey"] = "";
+    }
+
+    const node = await nodeService.createNode(data.node, data.privateKey);
+
+    response.send(node);
+  } catch (e) {
+    const error = {
+      message: e.toString()
+    };
+
+    response.status(400).send(error);
+  }
+});
+
+
+export default router;
