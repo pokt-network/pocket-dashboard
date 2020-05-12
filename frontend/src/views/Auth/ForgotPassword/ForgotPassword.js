21a109008635088d3ac5b3de84409d09eb21a8fb
Roniel Valdez <roniel_valdez@outlook.com>
2020-05-12 19:01:14 -0400

    integrating UI

--- frontend/src/views/Auth/ForgotPassword/ForgotPassword.js
+++ frontend/src/views/Auth/ForgotPassword/ForgotPassword.js
@@ -7,24 +7,30 @@ class ForgotPassword extends Component {
   render() {
     return (
       <Container fluid id={"forgot-password-page"}>
-        <Navbar/>
+        <Navbar />
         <Row>
-          <Col
-            id={"main"}
-            md={{span: 8, offset: 2}}
-            lg={{span: 4, offset: 4}}
-          >
-            <h1>Forgot your password?</h1>
-            <p>Write your email and we will send you a validation message</p>
-            <Form id={"main-form"}>
-              <Form.Group>
-                <Form.Label>E-mail</Form.Label>
-                <Form.Control type="email"/>
-              </Form.Group>
-              <Button type="submit" variant="dark" size={"lg"} block>
-                Send email
-              </Button>
-            </Form>
+          <Col id={"main"} md={{span: 8, offset: 2}} lg={{span: 4, offset: 3}}>
+            <div className="head"></div>
+            <div className="wrapper">
+              <h1 className="text-uppercase">Forgot your password?</h1>
+              <p className="font-weight-light">
+                Write your email and we will send you a validation message
+              </p>
+              <Form id={"main-form"}>
+                <Form.Group>
+                  <Form.Label id="email-label">Email Address</Form.Label>
+                  <Form.Control type="email" />
+                </Form.Group>
+                <Button
+                  className="pt-2 pb-2 pl-5 pr-5"
+                  type="submit"
+                  variant="primary"
+                  size={"md"}
+                >
+                  Reset password
+                </Button>
+              </Form>
+            </div>
           </Col>
         </Row>
       </Container>
