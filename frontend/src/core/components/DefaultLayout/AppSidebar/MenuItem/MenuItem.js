797eb8c1690e92ede3274b914dd064ffd7af263a
Santiago de Pedro <santiago.pedro23@gmail.com>
2020-05-16 18:44:48 -0400

    Fixing home ui issues.

--- frontend/src/core/components/DefaultLayout/AppSidebar/MenuItem/MenuItem.js
+++ frontend/src/core/components/DefaultLayout/AppSidebar/MenuItem/MenuItem.js
@@ -12,13 +12,7 @@ class MenuItem extends Component {
         <li>
           <img src={iconImageSource} alt="side-bar-menu-item-icon"/>
           <span>{label}</span>
-          <img
-            style={{width: "10%"}}
-            src={`/assets/${icon}.svg`}
-            className="icon"
-            alt=""
-          />
-          {label}
+
         </li>
       </NavLink>
     );
