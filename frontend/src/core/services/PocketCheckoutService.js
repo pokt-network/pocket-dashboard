f2997faf2befa40ef8209ae960819e9fdce3d452
Santiago de Pedro <santiago.pedro23@gmail.com>
2020-05-23 16:19:13 -0400

    Adding simple checkout api client.

---
+++ frontend/src/core/services/PocketCheckoutService.js
@@ -0,0 +1,47 @@
+import PocketBaseService from "./PocketBaseService";
+import axios from "axios";
+
+export class PocketCheckoutService extends PocketBaseService {
+  constructor() {
+    super("api/checkout");
+  }
+
+  /**
+   * Get relays per day data.
+   *
+   * @returns {Promise<object|*>} Relays per day data.
+   */
+  getRelaysPerDay() {
+    return axios
+      .get(this._getURL("relays-per-day"))
+      .then((response) => response.data);
+  }
+
+  /**
+   * Get account balance.
+   *
+   * @param {string} accountAddress Account address in hex to query.
+   *
+   * @returns {Promise<number|*>} The balance of account.
+   */
+  getAccountBalance(accountAddress) {
+    return axios
+      .get(this._getURL(`/balance/${accountAddress}`))
+      .then((response) => response.data);
+  }
+
+  /**
+   * Get money to spent.
+   *
+   * @param {number} relaysPerDay Relays per day.
+   *
+   * @returns {Promise<number|*>} Cost to spent.
+   */
+  getMoneyToSpent(relaysPerDay) {
+    return axios
+      .get(this._getURL(`/cost?rpd=${relaysPerDay}`))
+      .then((response) => response.data);
+  }
+}
+
+export default new PocketCheckoutService();
