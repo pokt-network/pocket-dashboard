fec1c6eb60ad9b40c2757e8e563d31d72360fdfa
Rafael De Leon <rafael.deleon@outlook.com>
2020-05-27 18:30:05 -0400

    Fixed lint issues.

--- frontend/src/views/Apps/AppsMain/AppsMain.js
+++ frontend/src/views/Apps/AppsMain/AppsMain.js
@@ -47,8 +47,7 @@ class AppsMain extends Main {
     const userEmail = UserService.getUserInfo().email;
 
     ApplicationService.getAllUserApplications(
-      userEmail,
-      APPLICATIONS_LIMIT
+      userEmail, APPLICATIONS_LIMIT
     ).then((userItems) => {
       ApplicationService.getStakedApplicationSummary().then(
         ({totalApplications, averageRelays, averageStaked}) => {
@@ -75,9 +74,7 @@ class AppsMain extends Main {
     this.setState({allItemsTableLoading: true});
 
     const registeredItems = await ApplicationService.getAllApplications(
-      APPLICATIONS_LIMIT,
-      0,
-      BOND_STATUS_STR[option]
+      APPLICATIONS_LIMIT, 0, BOND_STATUS_STR[option]
     );
 
     this.setState({registeredItems, allItemsTableLoading: false});
@@ -89,10 +86,7 @@ class AppsMain extends Main {
     const userEmail = UserService.getUserInfo().email;
 
     const userItems = await ApplicationService.getAllUserApplications(
-      userEmail,
-      APPLICATIONS_LIMIT,
-      0,
-      BOND_STATUS_STR[option]
+      userEmail, APPLICATIONS_LIMIT, 0, BOND_STATUS_STR[option]
     );
 
     this.setState({
@@ -106,9 +100,7 @@ class AppsMain extends Main {
     const {userItems} = this.state;
     const userEmail = UserService.getUserInfo().email;
     const newUserItems = await ApplicationService.getAllUserApplications(
-      userEmail,
-      APPLICATIONS_LIMIT,
-      offset * APPLICATIONS_LIMIT + 1
+      userEmail, APPLICATIONS_LIMIT, offset * APPLICATIONS_LIMIT + 1
     );
 
     const allUserItems = [...userItems, ...newUserItems];
@@ -124,8 +116,7 @@ class AppsMain extends Main {
     const {registeredItems} = this.state;
 
     const newRegisteredItems = await ApplicationService.getAllApplications(
-      APPLICATIONS_LIMIT,
-      offset * APPLICATIONS_LIMIT + 1
+      APPLICATIONS_LIMIT, offset * APPLICATIONS_LIMIT + 1
     );
 
     const allRegisteredItems = [...registeredItems, ...newRegisteredItems];
