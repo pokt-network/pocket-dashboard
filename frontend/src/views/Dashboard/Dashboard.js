4cdff47245d9e57e2e7e498f6eb1cb71530de1a7
Rafael De Leon <rafael.deleon@outlook.com>
2020-05-27 08:36:06 -0400

    Fixed lint issues.

--- frontend/src/views/Dashboard/Dashboard.js
+++ frontend/src/views/Dashboard/Dashboard.js
@@ -46,8 +46,7 @@ class Dashboard extends Component {
     const userEmail = UserService.getUserInfo().email;
 
     const userApps = await ApplicationService.getAllUserApplications(
-      userEmail,
-      APPLICATIONS_LIMIT
+      userEmail, APPLICATIONS_LIMIT
     );
     // const userNodes = await NodeService.getAllUserNodes(userEmail, NODES_LIMIT);
     const chains = await NetworkService.getAvailableNetworkChains();
