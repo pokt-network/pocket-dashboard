d2b9efecc5c95e9faa9fced6164e5959a9eb5b12
Arturo Ruiz <arturoruiz@Arturos-MacBook-Pro-4.local>
2020-05-29 22:54:44 -0400

    Initial commit of ToS page

--- frontend/src/views/Support/TermsOfService.js
+++ frontend/src/views/Support/TermsOfService.js
@@ -1,9 +1,229 @@
 import React, {Component} from "react";
+import "../Support/TermsOfService.scss";
 
+import {Button, Col, Container, Row} from "react-bootstrap";
 class TermsOfService extends Component {
   state = {};
   render() {
-    return <h1>Terms of Service</h1>;
+    return (
+      <Container fluid id="privacy-policy">
+        <Row>
+          <div className="address offset-lg-4">
+            <p className="">POCKET NETWORK, INC. </p>
+            <p>POKT TOKEN PURCHASE AGREEMENT</p>
+          </div>
+        </Row>
+        <Row>
+          <div className="policy">
+            NOTICE: ​THE TERMS OF THIS AGREEMENT FORM A BINDING LEGAL CONTRACT
+            BETWEEN YOU AND POCKET NETWORK, INC (THE “COMPANY”). CAREFULLY READ
+            ALL OF THE TERMS OF THIS AGREEMENT BEFORE CLICKING THE “I AGREE”
+            BUTTON. BY CLICKING THE “I AGREE” BUTTON YOU ACKNOWLEDGE YOUR
+            CONSENT AND AGREEMENT TO ALL THE TERMS AND CONDITIONS SET FORTH IN
+            THIS AGREEMENT. IF YOU DO NOT AGREE TO ALL THE TERMS OF THIS
+            AGREEMENT, DO NOT CLICK “I AGREE.” IF YOU HAVE ANY QUESTIONS
+            REGARDING THE EFFECT OF THE TERMS AND CONDITIONS IN THIS AGREEMENT,
+            YOU ARE ADVISED TO CONSULT INDEPENDENT LEGAL COUNSEL.
+          </div>
+          <br />
+        </Row>
+        <Row>
+          <div className="collect">
+            <span>Summary</span>
+            <p>
+              Review this POKT Token Purchase Agreement following your
+              completion of certain questions on our online website platform
+              http://dashboard.pokt.network/ (the “Purchasing Site”). If your
+              responses remain accurate and correct, click the check box and the
+              “I AGREE” button to indicate your agreement. Purchase Price (USD):
+              [____] [Purchase Price (BTC): [____] (BTC/USD Exchange Rate:
+              [____])] [Purchase Price (ETH): [____] (ETH /USD Exchange Rate:
+              [____])] Number of POKT Tokens (Purchased Tokens): [____]
+            </p>
+          </div>
+        </Row>
+        <Row>
+          <div className="information">
+            <span>PREAMBLE</span>
+            <p>
+              This Token Purchase Agreement (this “Agreement”) contains the
+              terms and conditions that govern your purchase of the POKT Tokens
+              (the “Tokens” or “POKT Tokens”), and it defines your rights and
+              obligations with respect to the purchased Tokens. This is an
+              agreement between you (“Purchaser” or “you”) and Pocket Network,
+              Inc, a Delaware corporation (the “Company”). Purchaser and the
+              Company are herein referred to individually as a “Party” and
+              collectively as the “Parties.”
+            </p>
+            <p>
+              <b>WHEREAS​,</b>​ the Company created and minted the Tokens, which
+              are a native digital asset intended to interact with and enable
+              use of the Company’s blockchain application, the Pocket Network
+              (the “Network”);
+            </p>
+            <p>
+              <b>WHEREAS​,</b>​ the Company is collaborating with Pocket Network
+              Foundation, an ownerless foundation company organized under the
+              laws of the Cayman Islands, to develop and manage the Network;
+            </p>
+            <p>
+              <b>WHEREAS​,</b> Purchaser desires to participate in the Network
+              either as an application developer (“Developer”) or as a Pocket
+              node (“Node”);
+            </p>
+            <p>
+              <b>Cookies.</b>​ We may log information using "cookies." Cookies
+              are small data files stored on your hard drive by a website. We
+              may use both session Cookies (which expire once you close your web
+              browser) and persistent Cookies (which stay on your computer until
+              you delete them) to provide you with a more personal and
+              interactive experience on our Site. This type of information is
+              collected to make the Site more useful to you and to tailor the
+              experience with us to meet your special interests and needs.
+            </p>
+          </div>
+        </Row>
+        <Row>
+          <div>
+            <span>USE OF PERSONAL INFORMATION</span>
+            <p></p>
+          </div>
+        </Row>
+        <Row>
+          <div>
+            We use your personal information as follows:
+            <ul>
+              <li>
+                We use your personal information to operate, maintain, and
+                improve our sites, products, and services.
+              </li>
+              <li>
+                We use your personal information to respond to comments and
+                questions and provide customer service.
+              </li>
+              <li>
+                We use your personal information to send information including
+                confirmations, invoices, technical notices, updates, security
+                alerts, and support and administrative messages.
+              </li>
+              <li>
+                We use your personal information to send information including
+                confirmations, invoices, technical notices, updates, security
+                alerts, and support and administrative messages.
+              </li>
+              <li>
+                We use your personal information to communicate about
+                promotions, upcoming events, and other news about products and
+                services offered by us and our selected partners.
+              </li>
+              <li>
+                We use your personal information to link or combine user
+                information with other personal information.
+              </li>
+              <li>
+                We use your personal information to protect, investigate, and
+                deter against fraudulent, unauthorized, or illegal activity.
+              </li>
+              <li>
+                We use your personal information to provide and deliver products
+                and services customers request.
+              </li>
+            </ul>
+          </div>
+        </Row>
+        <Row>
+          <div className="information">
+            <span>SHARING OF PERSONAL INFORMATION</span>
+          </div>
+        </Row>
+        <Row>
+          <div className="information">
+            <ul>
+              <p>We may share personal information as follows:</p>
+              <li>
+                We may share personal information with your consent. For
+                example, you may let us share personal information with others
+                for their own marketing uses. Those uses will be subject to
+                their privacy policies.
+              </li>
+              <li>
+                We may share personal information when we do a business deal, or
+                negotiate a business deal, involving the sale or transfer of all
+                or a part of our business or assets. These deals can include any
+                merger, financing, acquisition, or bankruptcy transaction or
+                proceeding.
+              </li>
+              <li>
+                We may share personal information for legal, protection, and
+                safety purposes.
+              </li>
+              <ul>
+                <li>We may share information to comply with laws.</li>
+                <li>
+                  We may share information to respond to lawful requests and
+                  legal processes.
+                </li>
+                <li>
+                  We may share information to protect the rights and property of
+                  Pocket Network, Inc., our agents, customers, and others. This
+                  includes enforcing our agreements, policies, and terms of use.
+                </li>
+                <li>
+                  We may share information in an emergency. This includes
+                  protecting the safety of our employees and agents, our
+                  customers, or any person.
+                </li>
+              </ul>
+              <li>
+                We may share information with those who need it to do work for
+                us.
+              </li>
+              <p>
+                We may also share aggregated and/or anonymized data with others
+                for their own uses.
+              </p>
+            </ul>
+          </div>
+        </Row>
+        <Row>
+          <div className="information">
+            <span>INFORMATION CHOICES AND CHANGES</span>
+            <p>
+              Our marketing emails tell you how to “opt-out.” If you opt out, we
+              may still send you non-marketing emails. Non-marketing emails
+              include emails about your accounts and our business dealings with
+              you. You may send requests about personal information to our
+              Contact Information below. You can request to change contact
+              choices, opt-out of our sharing with others, and update your
+              personal information. You can typically remove and reject cookies
+              from our Site with your browser settings. Many browsers are set to
+              accept cookies until you change your settings. If you remove or
+              reject our cookies, it could affect how our Site works for you.
+            </p>
+          </div>
+        </Row>
+        <Row>
+          <div className="information">
+            <span>CONTACT INFORMATION</span>
+            <p>
+              We welcome your comments or questions about this privacy policy.
+              You may also contact us at our address:
+              <p>Pocket Network, Inc.</p> <p>801 E Whiting St</p>
+              <p>Tampa, Florida 33602</p>
+            </p>
+          </div>
+        </Row>
+        <Row>
+          <div className="information">
+            <span>CHANGES TO THIS PRIVACY POLICY</span>
+            <p>
+              We may change this privacy policy. If we make any changes, we will
+              change the Last Updated date above.
+            </p>
+          </div>
+        </Row>
+      </Container>
+    );
   }
 }
 
