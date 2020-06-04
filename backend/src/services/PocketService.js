1a1f8d851f5dd20d280069cfc46ad11aa648a760
Santiago de Pedro <santiago.pedro23@gmail.com>
2020-06-04 13:01:56 -0400

    Disable tests per new Pocket service change.

--- backend/src/services/PocketService.js
+++ backend/src/services/PocketService.js
@@ -318,7 +318,7 @@ export default class PocketService {
    */
   async getApplications(status) {
     const pocketRpcProvider = await this.getPocketRPCProvider();
-    const applicationsResponse = await this.__pocket.rpc(pocketRpcProvider).query.getApps(status);
+    const applicationsResponse = await this.__pocket.rpc(pocketRpcProvider).query.getApps(status, 0n);
 
     if (applicationsResponse instanceof Error) {
       throw applicationsResponse;
