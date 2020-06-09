db32aa9b212d06a49aac38d66f223a815d4f7434
Arturo Ruiz <arturoruiz@Arturos-MacBook-Pro-4.local>
2020-06-09 18:19:45 -0400

    Add dynamic call to get the Pokt Price

--- frontend/src/views/Support/TermsOfService.js
+++ frontend/src/views/Support/TermsOfService.js
@@ -4,9 +4,20 @@ import {withRouter} from "react-router-dom";
 import {Container, Row} from "react-bootstrap";
 import Navbar from "../../core/components/Navbar";
 import {scrollToId} from "../../_helpers";
+import PocketNetworkService from "../../core/services/PocketNetworkService.js";
 
 class TermsOfService extends Component {
-  state = {};
+  constructor(props, context) {
+    super(props, context);
+    this.state = {poktPrice: ""};
+  }
+  async componentDidMount() {
+    const price = await PocketNetworkService.getNetworkSummaryData();
+
+    this.setState({poktPrice: price.poktPrice});
+  }
+
+  function;
   render() {
     return (
       <Container fluid id="privacy-policy">
@@ -48,7 +59,7 @@ class TermsOfService extends Component {
                 http://dashboard.pokt.network/ (the “Purchasing Site”). If your
                 responses remain accurate and correct, click the check box and
                 the “I AGREE” button to indicate your agreement. <br />
-                Purchase Price (USD): 0.06
+                Purchase Price (USD): {this.state.poktPrice}
                 <br />
                 Number of POKT Tokens (Purchased Tokens): [____]
               </p>
