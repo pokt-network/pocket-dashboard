e7bca5a19e7ab3686a272a4db6008f564e8aa75b
Arturo Ruiz <arturoruiz@Arturos-MacBook-Pro-4.local>
2020-06-22 19:42:00 -0400

    Initial commit handle Subscribe & unsubscribe

--- frontend/src/views/Support/Unsubscribe.js
+++ frontend/src/views/Support/Unsubscribe.js
@@ -5,7 +5,65 @@ import {Container, Row, Button} from "react-bootstrap";
 import Navbar from "../../core/components/Navbar";
 
 class Unsubscribe extends Component {
-  state = {};
+  constructor(props, context) {
+    super(props, context);
+
+    this.validateUnsubscribe = this.validateUnsubscribe.bind(this);
+
+    this.state = {
+      email: "",
+      alertOverlay: {
+        show: false,
+        variant: "",
+        message: "",
+      },
+    };
+  }
+
+  validateSubscribe() {
+    PocketUserService.subscribeUser(queryParam.d).then((result) => {
+      if (result.success) {
+        this.setState({email: result.data});
+      } else {
+        this.setState({
+          alertOverlay: {
+            show: true,
+            variant: "danger", // TODO update this variant to non danger
+            message: "You've been subscribed again.",
+          },
+        });
+      }
+    });
+  }
+
+  componentDidMount() {
+    // eslint-disable-next-line react/prop-types
+    const queryParam = qs.parse(this.props.location.search, {
+      ignoreQueryPrefix: true,
+    });
+
+    if (queryParam === undefined || queryParam.d === undefined) {
+      this.setState({
+        alertOverlay: {show: true, variant: "danger", message: "Invalid URL"},
+      });
+      return;
+    }
+
+    PocketUserService.unsubscribeUser(queryParam.d).then((result) => {
+      if (result.success) {
+        this.setState({email: result.data});
+      } else {
+        this.setState({
+          alertOverlay: {
+            show: true,
+            variant: "danger",
+            message: "We couldn't find a corresponding email to unsbuscribe.",
+          },
+        });
+      }
+    });
+  }
+
   render() {
     return (
       <Container fluid id="privacy-policy">
@@ -27,7 +85,13 @@ class Unsubscribe extends Component {
                   Didn&apos;t mean to unsubscribe?
                 </span>
               </p>
-              <Button className="btn-size " size="sm" variant="primary" block>
+              <Button
+                className="btn-size"
+                onChange={this.validateSubscribe}
+                size="sm"
+                variant="primary"
+                block
+              >
                 <span className="sign-up-btn">Subscribe again</span>
               </Button>
             </div>
