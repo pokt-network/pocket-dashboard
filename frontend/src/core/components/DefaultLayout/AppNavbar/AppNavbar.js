487d4813d6b2a0ba4730f8bcf5b816e826cc452e
Wilson <garcia.m.wilson@gmail.com>
2020-09-30 21:05:22 -0400

    Adding beta tag

--- frontend/src/core/components/DefaultLayout/AppNavbar/AppNavbar.js
+++ frontend/src/core/components/DefaultLayout/AppNavbar/AppNavbar.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
-import {Row} from "react-bootstrap";
+import {Button, Row} from "react-bootstrap";
+import {Link} from "react-router-dom";
 import PropTypes from "prop-types";
 import LoginStatus from "../LoginStatus/LoginStatus";
 import "./AppNavbar.scss";
@@ -14,9 +15,16 @@ class AppNavbar extends Component {
           <area target="_blank" shape="rect" coords="0,11,115,65" alt="Pocket" href="https://pokt.network" />
           <area shape="rect" coords="120,14,225,65" alt="Dashboard" href={`${window.location.origin}/dashboard`} />
         </map>
-        <div id="main-logo">
+        <div id="main-logo" style={{display: "inline-flex"}}>
           <img src="/assets/logo-dashboard-color.svg" alt="logo" useMap="#workmap"/>
+          <Button
+            variant="dark" style={{marginLeft: "40px", marginTop: "20px"}}>
+            <Link target="_blank" to="https://pokt.network/dashboard-beta-version-sugestion-box/" style={{color:"#FFFFFF"}}>
+              BETA
+              </Link>
+          </Button>
         </div>
+        
         <div className="items">
           {/* TODO: Uncomment when second release*/}
           {/*<div className="buttons">*/}
