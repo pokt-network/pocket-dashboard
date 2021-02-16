fabff70ae2e84e6ef7f402d39e5a3a47bd1d86ab
Enrique Ortiz <hi@enriqueortiz.dev>
2021-02-16 10:06:57 -0400

    chore: remove old components, export proper ui components

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,65 +1,12 @@
-import React, { Component } from "react";
-// noinspection ES6CheckImport
-import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
-import "./App.scss";
-import UserService from "./core/services/PocketUserService";
-import { MobileView, BrowserView } from "react-device-detect";
-//Pages
-import routes, { ROUTE_PATHS } from "./_routes";
+import React from "react";
+import { HashRouter as Router, Switch } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    const isUserLoggedIn = UserService.isLoggedIn();
-    const redirectTo =
-      isUserLoggedIn && window.location.pathname === "/"
-        ? ROUTE_PATHS.home
-        : ROUTE_PATHS.login;
-
-    return (
-      <>
-        <MobileView>
-          <div className="app-mobile">
-            <center>
-              <div className="vertical-center">
-                <img src="/assets/no-mobile.svg" alt="" className="indicator" />
-                <p className="title">
-                  Your Screen is <br /> too small
-                </p>
-                <p className="sub-title">
-                  Please acceess the
-                  <br /> Pocket Dashboard from a <br /> desktop device.{" "}
-                </p>
-              </div>
-            </center>
-          </div>
-          <img
-            src="/assets/preview.svg"
-            alt=""
-            style={{ width: "100%", height: "100%" }}
-          />
-        </MobileView>
-        <BrowserView>
-          <BrowserRouter>
-            <Switch>
-              {routes.map((route, idx) => {
-                return (
-                  <Route
-                    key={idx}
-                    path={route.path}
-                    exact={route.exact}
-                    name={route.name}
-                    component={route.component}
-                  />
-                );
-              })}
-
-              <Redirect from="/" to={redirectTo} />
-            </Switch>
-          </BrowserRouter>
-        </BrowserView>
-      </>
-    );
-  }
+function App() {
+  return (
+    <Router>
+      <Switch>Hi!</Switch>
+    </Router>
+  );
 }
 
 export default App;
--- packages/frontend/src/ui/index.js
+++ packages/frontend/src/ui/index.js
@@ -1,3 +1,28 @@
-import Button from "./Button/Button";
+import AppWrapper from "ui/AppWrapper/AppWrapper";
+import Button from "ui/Button/Button";
+import ButtonBase from "ui/ButtonBase/ButtonBase";
+import CircleGraph from "ui/CircleGraph/CircleGraph";
+import Help from "ui/Help/Help";
+import Link from "ui/Link/Link";
+import Popover from "ui/Popover/Popover";
+import Switch from "ui/Switch/Switch";
+import TextCopy from "ui/TextCopy/TextCopy";
+import TextInput from "ui/TextInput/TextInput";
 
-export { Button };
+// Export components
+export {
+  AppWrapper,
+  Button,
+  ButtonBase,
+  CircleGraph,
+  Help,
+  Link,
+  Popover,
+  Switch,
+  TextCopy,
+  TextInput,
+};
+
+// Theme, utils
+export * from "ui/theme";
+export * from "ui/utils";
