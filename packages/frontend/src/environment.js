19057b6a4f79a0b00ee10fb422255438960b984d
Enrique Ortiz <hi@enriqueortiz.dev>
2021-02-16 22:21:23 -0400

    chore: move sentry setup to its own dedicated file

--- packages/frontend/src/environment.js
+++ packages/frontend/src/environment.js
@@ -1,14 +1,17 @@
 const ENV_VARS = {
-  TIMESCALE_DB_ENDPOINT() {
-    return process.env.RECT_APP_TIMESCALEDB_ENDPOINT ?? "";
+  BUILD() {
+    return process.env.REACT_APP_BUILD || "undefined";
+  },
+  PROD() {
+    return process.env.NODE_ENV === "production";
   },
   SENTRY_DSN() {
     const dsn = process.env.REACT_APP_SENTRY_DSN ?? "";
 
     return dsn.trim();
   },
-  PROD() {
-    return process.env.NODE_ENV === "production";
+  TIMESCALE_DB_ENDPOINT() {
+    return process.env.RECT_APP_TIMESCALEDB_ENDPOINT ?? "";
   },
 };
 
